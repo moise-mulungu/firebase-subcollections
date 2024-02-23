c9e7166783d8e7f56dc4a0cd81b0a61d3e58024d
moise <moisemlg90@gmail.com>
2024-02-23 21:05:22 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+// import "./App.css";
+import { collection } from '@firebase/firestore'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+import { db } from './firebase'
+import ChildrenList from './ChildrenList'
+import AddNew from './AddNew'
+
+export default function App() {
+  const query = collection(db, 'oses')
+  const [docs, loading, error] = useCollectionData(query)
+
+  return (
+    <div>
+      <h1>Operating Systems</h1>
+
+      {loading && 'Loading...'}
+
+      <ul>
+        {docs?.map((doc) => (
+          <div key={Math.random()}>
+            <li>{doc.name}</li>
+
+            <ChildrenList path={`oses/${doc.name}/children`} />
+          </div>
+        ))}
+        <AddNew path="oses" />
+      </ul>
+    </div>
+  )
+}
